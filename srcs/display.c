25aee3154696445ad6e636c86b41f03a76c54a3c
Sébastien Nicolet <snicolet@student.42.fr>
2016-11-15 17:00:58 +0100

    fix du zoom

--- srcs/display.c
+++ srcs/display.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/22 13:18:49 by snicolet          #+#    #+#             */
-/*   Updated: 2016/11/14 22:05:48 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/11/15 17:00:21 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 #include "geo.h"
 #include "quaternion.h"
 
-static void			matrix_keyboard(GLFWwindow *window, t_v4d *scale,
-	t_quaternion *q)
+static void			matrix_keyboard(GLFWwindow *window, t_quaternion *q,
+	t_v4d *cam)
 {
 	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-		*scale = geo_addv4(*scale, (t_v4d){0.01, 0.01, 0.01, 0.0});
+		cam->z += 0.1;
 	else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-		*scale = geo_addv4(*scale, (t_v4d){-0.01, -0.01, -0.01, 0.0});
+		cam->z -= 0.1;
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		*q = geo_quat_mult(*q, geo_quat_rot((t_v3d){0.0, 1.0, 0.0}, 0.02));
 	else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
@@ -37,24 +37,18 @@ static void			matrix_keyboard(GLFWwindow *window, t_v4d *scale,
 
 static t_m4			make_matrix(GLFWwindow *window)
 {
+	static t_v4d			camera = (t_v4d){0.0, 0.0, -15.0, 0.0};
 	static double			rot = 0;
-	static t_v4d			scale = (t_v4d){0.7, 0.7, 0.7, 0.0};
 	t_m4					m;
 	static t_quaternion		q = (t_quaternion){1.0, 0.0, 0.0, 0.0};
 
-	//q = geo_quat_mult(q, geo_quat_rot((t_v3d){1.0, 0.0, 0.0}, 0.02));
-	//q = geo_quat_mult(q, geo_quat_rot((t_v3d){0.0, 1.0, 0.0}, 0.01));
-	matrix_keyboard(window, &scale, &q);
+	matrix_keyboard(window, &q, &camera);
 	if (rot > 10000)
 		rot = 0.0;
 	else
 		rot += 0.012;
-	m = geo_mk4_rotxyz(
-		(t_v4d){0.0, 0.0, 0.0, 0.0},
-		scale,
-		(t_v4d){0.0, 0.0, 0.0, 1.0});
-	m = geo_mult_m4(geo_quat_tomatrix(q), &m);
-	m.w.z -= 15.0;
+	m = geo_quat_tomatrix(q);
+	m.w.z = camera.z;
 	return (m);
 }
 
