f40c990fde3b52401aa21dc8d777b1e60705dc98
Map <blendfan@gmail.com>
2017-05-26 18:10:21 +0200

    fix uv in parser duplicate

--- srcs/parser_duplicate.c
+++ srcs/parser_duplicate.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/26 17:39:06 by snicolet          #+#    #+#             */
-/*   Updated: 2017/05/26 17:50:00 by qloubier         ###   ########.fr       */
+/*   Updated: 2017/05/26 18:07:48 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,11 @@ int						parse_duplicate(t_vertex_pack *pack)
 	{
 		idx = &pack->faces[p];
 		*(i) = pack->items[idx->x];
-		(i++)->uv = pack->items[pack->fuv[p].x].uv;
+		(i++)->uv = pack->uv[pack->fuv[p].x];
 		*(i) = pack->items[idx->y];
-		(i++)->uv = pack->items[pack->fuv[p].y].uv;
+		(i++)->uv = pack->uv[pack->fuv[p].y];
 		*(i) = pack->items[idx->z];
-		(i++)->uv = pack->items[pack->fuv[p].z].uv;
+		(i++)->uv = pack->uv[pack->fuv[p].z];
 		*idx = (t_v3i){vidx, vidx + 1, vidx + 2};
 		vidx += 3;
 		p++;
