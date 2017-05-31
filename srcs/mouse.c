80dd4e8406cbddca90a9dbad674a13c0851d5147
Chr0nos <snicolet@student.42.fr>
2017-05-31 17:11:48 +0000

    avancement sur la possibilite de tourner la camera avec un clic de la souris enfoncee

--- srcs/mouse.c
+++ srcs/mouse.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/31 12:52:00 by snicolet          #+#    #+#             */
-/*   Updated: 2017/05/31 13:16:12 by snicolet         ###   ########.fr       */
+/*   Updated: 2017/05/31 17:08:35 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void		mouse_pos_callback(GLFWwindow* window, double xpos, double ypos)
 {
 	const t_v2i			pos = (t_v2i){(int)xpos, (int)ypos};
 	static t_v2i		last = (t_v2i){0, 0};
+	t_v4d				offset;
 	t_v2i				vec;
 	t_vertex_pack		*pack;
 
@@ -24,6 +25,13 @@ void		mouse_pos_callback(GLFWwindow* window, double xpos, double ypos)
 	if (pack->input & INPUT_LCLICK)
 	{
 		vec = (t_v2i){pos.x - last.x, pos.y - last.y};
+		offset = pack->camera.w;
+		if (vec.x != 0)
+			pack->camera_quat = geo_quat_mult(pack->camera_quat, geo_quat_rot((t_v3d){0.0, 1.0, 0.0}, 0.002 * vec.x));
+		if (vec.y != 0)
+			pack->camera_quat = geo_quat_mult(pack->camera_quat, geo_quat_rot((t_v3d){1.0, 0.0, 0.0}, 0.002 * vec.y));
+		pack->camera.w = offset;
+		pack->camera = geo_quat_tomatrix(pack->camera_quat);
 		ft_printf("mouse move: %d %d -- %d %d\n", pos.x, pos.y, vec.x, vec.y);
 		last = pos;
 	}
