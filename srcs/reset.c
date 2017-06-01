192031c182bbe9beaebd9c1226497558b3fea5ce
Chr0nos <snicolet@student.42.fr>
2017-06-01 14:48:37 +0000

    C fix du reset de la camera (pour ne plus respawn a 0.0 en z(

--- srcs/reset.c
+++ srcs/reset.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/01 13:57:30 by snicolet          #+#    #+#             */
-/*   Updated: 2017/06/01 14:01:00 by snicolet         ###   ########.fr       */
+/*   Updated: 2017/06/01 14:48:25 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void		reset_camera(t_vertex_pack *pack)
 {
 	pack->camera_quat = geo_quat_identity();
 	pack->camera = geo_quat_tomatrix(pack->camera_quat);
+	pack->camera.w.z = -8;
 }
 
 void		reset_model(t_vertex_pack *pack)
