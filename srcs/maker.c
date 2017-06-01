058e554494430892afc3b57edaf382972e986692
Chr0nos <snicolet@student.42.fr>
2017-06-01 17:48:39 +0000

    ajout d une verification pour le link des shaders

--- srcs/maker.c
+++ srcs/maker.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/07 12:35:02 by snicolet          #+#    #+#             */
-/*   Updated: 2017/05/28 00:39:38 by snicolet         ###   ########.fr       */
+/*   Updated: 2017/06/01 17:48:27 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,8 +65,18 @@ static GLint		make_texture(GLuint *image_id, const char *name,
 	return (id);
 }
 
+static void			make_program_binds(t_vertex_pack *pack)
+{
+	glBindAttribLocation(pack->program, 0, "my_position");
+	glBindAttribLocation(pack->program, 1, "my_color");
+	glBindAttribLocation(pack->program, 2, "my_uv");
+	glBindAttribLocation(pack->program, 3, "my_normal");
+}
+
 int					make_program(t_vertex_pack *pack)
 {
+	int		link_ok;
+
 	ft_putendl("making program");
 	if (!(pack->fs = ft_shader_compile(GL_FRAGMENT_SHADER, "fragment.glsl")))
 		return (1);
@@ -74,13 +84,13 @@ int					make_program(t_vertex_pack *pack)
 		return (2);
 	ft_putendl("shaders ok");
 	pack->program = glCreateProgram();
-	glBindAttribLocation(pack->program, 0, "my_position");
-	glBindAttribLocation(pack->program, 1, "my_color");
-	glBindAttribLocation(pack->program, 2, "my_uv");
-	glBindAttribLocation(pack->program, 3, "my_normal");
+	make_program_binds(pack);
 	glAttachShader(pack->program, pack->fs);
 	glAttachShader(pack->program, pack->vs);
 	glLinkProgram(pack->program);
+	glGetProgramiv(pack->program, GL_LINK_STATUS, &link_ok);
+	if (!link_ok)
+		return (3);
 	glUseProgram(pack->program);
 	pack->texture_id = make_texture(&pack->texture, "texture_sampler",
 			pack->texture_path, 1);
