7d753ed0105016ad693a9f7702763a4f96e2fb49
Map <blendfan@gmail.com>
2018-10-01 15:48:03 +0200

    add comment to fragment shader

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -35,6 +35,7 @@ vec3		get_tangeant(void)
 {
 	vec3 tangeant;
 
+	// transform tangeant from local to global space
 	tangeant = inverse(mat3(view)) * mat3(model) * ftangeant;
 	return tangeant;
 }
@@ -55,24 +56,31 @@ vec3		get_normal(void)
 {
 	vec3	normal;
 
+	// transform normal from local to global space
 	normal = inverse(mat3(view)) * mat3(model) * fnormal;
 	if ((flags & FLAG_NMAP) == 0)
 		return normal;
+	// multiply normal map input with the tbn matrix
+	// from the normal in gobal space
 	return (get_tbn(normal) * (texture(normal_map, uv).rgb * 2.0 - 1.0));
 }
 
 float		make_brightness(void)
 {
 	vec3	fpos = fvertex.xyz;
+	// get light to pos vector
 	vec3	stl = fpos - light.position;
+	// get intensity based on light intensity parameter and distance from the light
 	float	brightness = light.intensity / clamp(length(stl), light.intensity, 500.0f);
+	// multiply the directional light calculatioin from the point light
+	// With the intensity computed before
 	return (max(dot(get_normal(), normalize(stl)), 0.0) *  brightness);
 }
 
 float		make_directional_brightness(vec3 direction)
 {
+	// dot between dir and normal in global space == directionale light
 	float	brightness = max(dot(get_normal(), direction), 0);
-
 	return (clamp(brightness, 0.0, 1.0));
 }
 
@@ -93,8 +101,8 @@ void		main() {
 				make_brightness(); // * ((texture(ambiant_occlusion, uv) + 2.0) / 3.0);
 		}
 		if (tex_switch > 0)
-			//color = mix(color, fcolor, clamp(tex_switch, 0, 1));
-			color.xyz = fvertex.xyz;
+			color = mix(color, fcolor, clamp(tex_switch, 0, 1));
+			//color.xyz = fvertex.xyz;
 	}
 	frag_color = color;
 }
